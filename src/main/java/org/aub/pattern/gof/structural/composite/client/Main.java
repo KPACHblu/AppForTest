0d59f8cf9af96ffb378a0e0569793a79de5bc4fa
KPACHblu <aubolut@gmail.com>
2014-12-22 22:06:03 +0200

    Added an example of the Composite pattern

--- src/main/java/org/aub/pattern/gof/structural/composite/client/Main.java
+++ src/main/java/org/aub/pattern/gof/structural/composite/client/Main.java
@@ -4,6 +4,12 @@ import org.aub.pattern.gof.structural.composite.Component;
 import org.aub.pattern.gof.structural.composite.Menu;
 import org.aub.pattern.gof.structural.composite.MenuItem;
 
+/**
+  The Composite Pattern allows you to compose
+  objects into tree structures to represent part-whole
+  hierarchies. Composite lets clients treat 
+  individual objects and compositions of objects uniformly.
+**/
 public class Main {
     public static void main(String[] args) {
         Component root = new Menu("Root");
